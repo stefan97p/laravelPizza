d5eae8744f4f4a63f85a3a0a40f822c582317622
Stefan Jokanovic <stefan97pro@gmail.com>
2022-09-21 19:59:05 +0200

    Css dodavanje galerije pizza

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -3,6 +3,16 @@
 @section('content')
 <div class="wrapper create-pizza">
     <h1>Pick your pizza!</h1>
+    <div class="gallery">
+        <div class="column"><img id="img2" src="/img/margarita.jpg" alt="margarta"/></div>
+        <div class="column"><img id="img3" src="/img/funghi.jpg" alt="funghi"/></div>
+        <div class="column"><img id="img4" src="/img/vegeteriano.jpg" alt="vegeteriano"/></div>
+        <div class="column"><img id="img1" src="/img/capricciosa.jpg" alt="capricciosa"/></div>
+        <div class="column"><img id="img5" src="/img/quatro.jpg" alt="quatro"/></div>
+        <div class="column"><img id="img2" src="/img/pepperoni.jpg" alt="pepperoni"/></div>
+        <div class="column"><img id="img2" src="/img/hawaiian.jpg" alt="hawaiian"/></div>
+        <div class="column"><img id="img2" src="/img/bbq.jpg" alt="bbq"/></div>
+    </div>
     <form action="/pizzas" method="POST">
         @csrf
         <label for="name">Name:</label>
@@ -16,6 +26,9 @@
             <option value="vegeteriano">VEGETERIANO</option>
             <option value="capricosa">CAPRICOSA</option>
             <option value="quatroformaggi">QUATRO FORMAGGI</option>
+            <option value="pepperoni">PEPPERONI</option>
+            <option value="hawaiian">HAWAIIAN</option>
+            <option value="bbq chicken">BBQ CHIKEN</option>
         </select>
         <label for="base">Size:</label>
         <select name="base" id="base">
@@ -29,8 +42,9 @@
             <input type="checkbox" name="toppings[]" value="pappers">Pappers </br>
             <input type="checkbox" name="toppings[]" value="olives">Olives </br>           
         </fieldset>
-        <input type="submit" value="Confirm your order">           
+        <input id="orderbtn" type="submit" value="Confirm your order">           
     </form>
+    
 </div>
     
 @endsection
